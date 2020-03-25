b7fef8d4b5bc0fc54a2d4cbf1f7ee82d838e075b
Jongchan Lee <jclee@JongchannoMacBook-Air.local>
2020-03-26 03:16:11 +0900

    45454

--- jcmmit.py
+++ jcmmit.py
@@ -2,9 +2,12 @@
 import os
 import re
 import sys
-
+from datetime import datetime
 
 commit_message=input("コミット入力")
+# commit_message=str(datetime.now())
+# commit_message=re.sub("\.|:", "-", commit_message)
+
 
 os.system("git add . ")
 os.system("git commit -m"+""+commit_message)
